e36ecffd861410b89da6489a7186c0188b21966a
Aaron Sutton <aaronjsutton@icloud.com>
2021-03-12 10:53:03 -0500

    Start ref impl

--- c4.c
+++ c4.c
@@ -3,7 +3,28 @@
   * Aaron Sutton
   */
 
+#include <stdio.h>
+#include <stdbool.h>
+
+// Simply print out a nice welcome message.
+void welcome() {
+  printf("Welcome to connect-4: C version\n");
+  printf("This is a 2 player game, each player will take turns placing a token.\n");
+  printf("The objective is to create a line of four consecutive tokens.\n");
+  printf("Good luck!\n");
+}
+
 int main(int argc, char* argv[]) {
-  
+  welcome();
+
+  int board[6][7] = {
+    {0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0},
+  };
+
   return 0;
 }
--- four.c
+++
@@ -1,9 +0,0 @@
-/* 
- * Connect Four - C reference implementation
- * Version 1.0
- * Aaron Sutton - ajs377@pitt.edu
- */
-
-int main(int argc, char *argv[]) {
-
-}
